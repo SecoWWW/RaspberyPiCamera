f0562f90769024915e9dcd613f1a5fb4a48b0366
SecoWWW <patrik.scensny@gmail.com>
2017-12-01 19:54:22 +0000

    new changes

--- python-scripts/example.py
+++ python-scripts/example.py
@@ -2,10 +2,16 @@
 
 from time import sleep
 from picamera import PiCamera
+import os
+
+
 
 camera = PiCamera()
+
 camera.resolution = (1024,768)
-camera.start_preview()
-# Camera warm-up time
+
+camera.hflip = True
+camera.vflip = True
+camera.preview
 sleep(2)
-camera.capture("foo.jpg")
+camera.capture([])
