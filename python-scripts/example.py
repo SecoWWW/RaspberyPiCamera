ff855e9e9616a02c501bd17f45c5cd17d1f077ec
KnucklePunch <patrik.scensny@gmail.com>
2017-12-01 20:55:18 +0100

    changed the file

--- python-scripts/example.py
+++ python-scripts/example.py
@@ -3,8 +3,12 @@
 from time import sleep
 from picamera import PiCamera
 
+
+
 camera = PiCamera()
 camera.resolution = (1024,768)
+camera.hflip = True
+camera.vflip = True
 camera.start_preview()
 # Camera warm-up time
 sleep(2)
